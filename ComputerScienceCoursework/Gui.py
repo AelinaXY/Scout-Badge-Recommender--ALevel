67891393cd3995688bbeaefb8c9fec5f273702ce
Stormstar109 <46847822+Stormstar109@users.noreply.github.com>
2019-03-27 18:57:34 +0000

    Update Menu to have proper recommended badges,
    Add images for 9 badges. Create framework for future problems

--- ComputerScienceCoursework/Gui.py
+++ ComputerScienceCoursework/Gui.py
@@ -24,15 +24,28 @@ def picker():
     return choiceDict
 
 
-def output(recs):
-    msg = ("The program believes you would like these badges: \n" + str(recs[0]) + "\n" + str(recs[1]) + "\n" + str(recs[2]) + "\n" + str(recs[3]) + "\n" + str(recs[4]))
-    title = "Recommendations"
+def recommendedBadges():
+    with open('recommendations.json') as json_file:
+        recommendations = list(json.load(json_file))
 
-    if ccbox(msg, title):
-        mainStartup()
-        pass
-    else:
-        sys.exit(0)
+        if len(recommendations) <= 5:
+            msgbox(msg="This application cannot help you as you know what you need to do. \n Go and do it, We believe in you!", title="Yay!")
+
+        print(recommendations[0])
+        image = "badges\{0}.png".format(recommendations[0])
+        choice = buttonbox("We believe that you would like {0}, {1}, {2}, {3}, {4} \n Click the badge you want to find out more about, or click back".format(recommendations[0], recommendations[1], recommendations[2], recommendations[3], recommendations[4]), title="Recommendations", choices=(["Back", recommendations[0], recommendations[1], recommendations[2], recommendations[3], recommendations[4]]), image=image)
+        if choice == "Back":
+            mainFunc()
+
+        if choice == image:
+            print(recommendations[0])
+            badgeFunc(recommendations[0])
+
+        for i in range(5):
+            if choice == recommendations[i]:
+                print(choice)
+                badgeFunc(recommendations[i])
+
 
 
 def newRecommendations():
@@ -43,19 +56,16 @@ def newRecommendations():
         tempRec = recommendations[i]
         rec1.append(tempRec[0])
 
-    print(rec1)
     for i in range(len(list(badges.keys()))):
         if (list(badges.values()))[i] == 1:
-            print ((list(badges.keys()))[i])
             rec1.remove((list(badges.keys()))[i])
 
     with open('recommendations.json', 'w') as f:
         json.dump(rec1, f)
 
 
-    print("help", rec1)
 
-    return rec1
+    return
 
 def mainFunc():
     image = "logo.png"
@@ -63,6 +73,8 @@ def mainFunc():
 
     if choice == "Enter new recommendations":
         newRecommendations()
+        recommendedBadges()
+
 
 
 mainFunc()
